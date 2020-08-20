40c58e16d2490900c196352396e34798af693aad
faizkhan01 <67521965+faizkhan01@users.noreply.github.com>
2020-08-21 03:22:52 +0600

    Dynamic Component Handling Done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1> My Heading : {(540*340)/230+1000} </h1>
+        <p> My First React Paragraph </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +20,31 @@ function App() {
         >
           Learn React
         </a>
+        <Person name= "Md Faizur Rahman Khan" profession= 'A Professional Web Developer'>  </Person>
+        <Person name= 'Md. Riad Hossain' profession= 'Junior Designer'> </Person>
+        <Person name='Md. Mahmudul Hasan' profession='Senior Graphics Designer'> </Person>
+        <Person name='Md. Faruk Patowary' profession='Junior Civil Engineer'> </Person>
+
+
       </header>
     </div>
   );
+}
+function Person(props){
+  const personStyle={
+    border: '4px solid cyan',
+    margin: '10px',
+    padding: '10px',
+    width: '50%',
+    backgroundColor: 'cyan'
+  }
+  return ( 
+    <div style={personStyle}>
+      <h1> Name: {props.name} </h1>
+      <h3> {props.profession}   </h3>
+    </div>
+
+  ) 
 }
 
 export default App;
